754ec20631921ddd2d791771713ee7a66a89e029
Soyoung Gwak <rhkrthdud7@gmail.com>
2020-02-23 14:38:08 +0900

    init

---
+++ UIBezierPathExample/CustomView.swift
@@ -0,0 +1,21 @@
+//
+//  CustomView.swift
+//  UIBezierPathExample
+//
+//  Created by Soso on 23/02/2020.
+//  Copyright © 2020 Soso. All rights reserved.
+//
+
+import UIKit
+
+class CustomView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ UIBezierPathExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIBezierPathExample
+//
+//  Created by Soso on 23/02/2020.
+//  Copyright © 2020 Soso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
