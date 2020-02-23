005edb41e514ceb92c8f4c15ac9a677ea491b8b5
Soyoung Gwak <rhkrthdud7@gmail.com>
2020-02-23 16:43:46 +0900

    move(), addLine(), addArc(), addCurve(), stroke(), setStroke()

--- UIBezierPathExample/CustomView.swift
+++ UIBezierPathExample/CustomView.swift
@@ -10,12 +10,30 @@ import UIKit
 
 class CustomView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
     override func draw(_ rect: CGRect) {
-        // Drawing code
+        super.draw(rect)
+        
+        let pathLine = UIBezierPath()
+        pathLine.move(to: .zero)
+        let center = CGPoint(x: bounds.width/2, y: bounds.height/2)
+        pathLine.addLine(to: center)
+        UIColor.blue.setStroke()
+        pathLine.stroke()
+        
+        let pathArc = UIBezierPath()
+        pathArc.addArc(withCenter: center, radius: bounds.height/2, startAngle: 0, endAngle: .pi*2, clockwise: true)
+        UIColor.red.setStroke()
+        pathArc.stroke()
+        
+        let pathCurve = UIBezierPath()
+        let downLeftCorner = CGPoint(x: 0, y: bounds.height)
+        pathCurve.move(to: downLeftCorner)
+        let upRightCorner = CGPoint(x: bounds.width, y: 0)
+        let point1 = CGPoint(x: 0, y: 0)
+        let point2 = CGPoint(x: bounds.width, y: bounds.height)
+        pathCurve.addCurve(to: upRightCorner, controlPoint1: point1, controlPoint2: point2)
+        UIColor.green.setStroke()
+        pathCurve.stroke()
     }
-    */
-
+    
 }
