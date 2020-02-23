8f481f78fc2c0e35acdee0095e9167cce1129946
Soyoung Gwak <rhkrthdud7@gmail.com>
2020-02-23 17:55:56 +0900

    fill(), setFill(), lineWidth

--- UIBezierPathExample/CustomView.swift
+++ UIBezierPathExample/CustomView.swift
@@ -13,27 +13,17 @@ class CustomView: UIView {
     override func draw(_ rect: CGRect) {
         super.draw(rect)
         
-        let pathLine = UIBezierPath()
-        pathLine.move(to: .zero)
-        let center = CGPoint(x: bounds.width/2, y: bounds.height/2)
-        pathLine.addLine(to: center)
-        UIColor.blue.setStroke()
-        pathLine.stroke()
+        let rect = bounds.insetBy(dx: 20, dy: 50)
+        let pathRect = UIBezierPath(rect: rect)
+        UIColor.lightGray.setStroke()
+//        pathRect.stroke()
         
-        let pathArc = UIBezierPath()
-        pathArc.addArc(withCenter: center, radius: bounds.height/2, startAngle: 0, endAngle: .pi*2, clockwise: true)
-        UIColor.red.setStroke()
-        pathArc.stroke()
-        
-        let pathCurve = UIBezierPath()
-        let downLeftCorner = CGPoint(x: 0, y: bounds.height)
-        pathCurve.move(to: downLeftCorner)
-        let upRightCorner = CGPoint(x: bounds.width, y: 0)
-        let point1 = CGPoint(x: 0, y: 0)
-        let point2 = CGPoint(x: bounds.width, y: bounds.height)
-        pathCurve.addCurve(to: upRightCorner, controlPoint1: point1, controlPoint2: point2)
-        UIColor.green.setStroke()
-        pathCurve.stroke()
+        let pathOval = UIBezierPath(ovalIn: rect)
+        UIColor.yellow.setFill()
+        UIColor.lightGray.withAlphaComponent(0.5).setStroke()
+        pathOval.lineWidth = 30
+        pathOval.fill()
+        pathOval.stroke()
     }
     
 }
